6119c5840a055902c7be4e374ad2aefcc8794772
Odne Stenseth <odne.stenseth@gmail.com>
2022-03-24 13:38:33 +0000

    Start on updating inspector pane

--- src/InspectorPane/InspectorContainer.tsx
+++ src/InspectorPane/InspectorContainer.tsx
@@ -2,16 +2,32 @@ import { Button } from "antd";
 import React, { useContext } from "react";
 import { globalContext } from "../App";
 import { globalContextType } from "../App";
+import { Input } from 'antd';
 
 const InspectorContainer = (props: Object) => {
   const context: globalContextType = useContext(globalContext);
-  const selection = context.fabricCanvas?.getActiveObject()
+  const selection: any | undefined = context.fabricCanvas?.getActiveObject()
+
+  const width: number | undefined = selection ? selection.width * selection.scaleX : undefined
+  const height: number | undefined = selection ? selection.height * selection.scaleY : undefined
+
   return (
     <>
-      <Button onClick={() => context.fabricCanvas?.clear()} type="primary">
-        Clear
-      </Button>
-      <pre>{selection && JSON.stringify(selection, null, 4)}</pre>
+      {!selection && 
+      <p>no element selected</p>
+      }
+
+      {selection &&
+      <>
+        <p>Dimensions</p>
+        <p>Width: </p>
+        <p>Height: </p>
+        <Input addonBefore="Width:" value={width} />
+        <Input addonBefore="Height:" value={height}/>
+
+        <pre>{selection && JSON.stringify(selection, null, 4)}</pre>
+      </>
+      }
     </>
   );
 };
