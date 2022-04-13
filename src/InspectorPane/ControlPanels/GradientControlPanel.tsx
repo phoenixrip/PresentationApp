836083099dc1d266fde7ecf6abda50cd2e7b2ea1
Odne Stenseth <odne.stenseth@gmail.com>
2022-04-13 23:30:08 +0100

    combined fill and gradient into one pane

--- src/InspectorPane/ControlPanels/GradientControlPanel.tsx
+++ src/InspectorPane/ControlPanels/GradientControlPanel.tsx
@@ -11,11 +11,11 @@ import './grapickCustom.css'
 import { ChromePicker } from 'react-color';
 
 interface Props {
-    selection: any | undefined
+    selection: any | undefined,
+    tickRadialModeSwitch: Boolean
 }
 
-
-const GradientControlPanel = ({ selection }: Props) => {
+const GradientControlPanel = ({ selection, tickRadialModeSwitch }: Props) => {
     const context: EditorContextTypes = useContext(editorContext);
     const setOnFabricObject: Function = context.setOnFabricObject
 
@@ -25,7 +25,6 @@ const GradientControlPanel = ({ selection }: Props) => {
     let refreshing = useRef(false)
 
     useEffect(() => {
-        console.log("mount me baby")
         gradientPicker.current = new Grapick({ el: '#gradientPicker' });
         if (selection.fill.type === "linear" || selection.fill.type === "radial") {
             for (const colorStop of selection.fill.colorStops) {
@@ -107,10 +106,9 @@ const GradientControlPanel = ({ selection }: Props) => {
     //refresh grapick on selection change
     useEffect(() => {
         refreshGradientPicker()
-    }, [selection])
+    }, [selection, tickRadialModeSwitch])
 
     const refreshGradientPicker = () => {
-        console.log("shit that's refreshing")
         refreshing.current = true
         gradientPicker.current.clear()
         if (selection.fill.type === "linear" || selection.fill.type === "radial") {
@@ -129,96 +127,7 @@ const GradientControlPanel = ({ selection }: Props) => {
 
     return (
         <>
-            <Radio checked={selection?.fill?.type === "linear"}
-                onClick={(e: any) => {
-                    const linearGradient = new fabric.Gradient({
-                        type: "linear",
-                        coords: {
-                            x1: 0,
-                            y1: 0,
-                            x2: selection.width,
-                            y2: selection.height,
-                        },
-                        colorStops: [
-                            {
-                                "offset": 0.72,
-                                "color": "#040404"
-                            },
-                            {
-                                "offset": 0.28,
-                                "color": "#6450c8"
-                            },
-                            {
-                                "offset": 0.13,
-                                "color": "#0d0827"
-                            },
-                            {
-                                "offset": 0.51,
-                                "color": "#a291f5"
-                            },
-                            {
-                                "offset": 0.28,
-                                "color": "#6450c8"
-                            },
-                            {
-                                "offset": 0.87,
-                                "color": "#e9e7f1"
-                            }
-                        ]
-
-                    })
-
-                    setOnFabricObject(selection, { fill: linearGradient }, "setGradient")
-                    refreshGradientPicker()
-                    selection.refreshGradientAngleControls()
-                }}
-            >Linear</Radio>
-            <Radio checked={selection?.fill?.type === "radial"}
-                onClick={(e: any) => {
-                    const radialGradient = new fabric.Gradient({
-                        type: "radial",
-                        coords: {
-                            r1: selection.height / 2 + selection.width / 4,
-                            r2: selection.width * .05,
-
-                            x1: selection.width / 2,
-                            y1: selection.height / 2,
-
-                            x2: selection.width / 2,
-                            y2: selection.height / 2
-                        },
-                        colorStops: [
-                            {
-                                "offset": 0.2,
-                                "color": "#fff"
-                            },
-                            {
-                                "offset": 0.32,
-                                "color": "#000"
-                            },
-                            {
-                                "offset": 0.45,
-                                "color": "rgba(110,89,215,1)"
-                            },
-
-                            {
-                                "offset": 0.64,
-                                "color": "rgba(54, 18, 234, 255)"
-                            },
-                            {
-                                "offset": 0.82,
-                                "color": "rgba(52,15,235,1)"
-                            },
-                        ]
-                    })
-                    setOnFabricObject(selection, { fill: radialGradient }, "setGradient")
-                    refreshGradientPicker()
-                    selection.refreshGradientAngleControls()
-                }}>Radial</Radio>
-
-            <p></p>
             <div id="gradientPicker"></div>
-            <p></p>
             <Button onClick={handleDeleteGrapickHandler}>Delete Color Stop</Button>
             <ChromePicker color={selectedGrapickHandler?.color}
                 onChangeComplete={(e) => selectedGrapickHandler?.setColor(`rgba(${e.rgb.r},${e.rgb.g},${e.rgb.b},${e.rgb.a})`)} />
