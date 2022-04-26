e398a737d2f09f7719287f43afa3bcbab5d0548c
robbiewhite <robbiewhite@mac.com>
2022-04-26 13:43:27 +0100

    Starting media picker plugins

---
+++ src/MediaPicker/MediaPickerContainer.tsx
@@ -0,0 +1,16 @@
+import { Modal } from "antd"
+
+interface Props {
+  open: boolean
+}
+const MediaPickerContainer = (props: Props) => {
+  return (
+    <Modal>
+
+    </Modal>
+  )
+}
+
+export {
+  MediaPickerContainer
+}
